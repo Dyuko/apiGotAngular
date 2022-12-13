5aa5de24e437ac9beaeabefb8067aad5adefe533
Dyuko <matias.irala.aveiro@gmail.com>
2022-12-13 15:08:36 -0300

    Agrega routes del módulo character-locations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterListComponent } from './modules/character-locations/character-list/character-list.component';
+import { SearchCharacterComponent } from './modules/character-locations/search-character/search-character.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'character-locations-list', component: CharacterListComponent },
+  { path: 'character-locations-search', component: SearchCharacterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
